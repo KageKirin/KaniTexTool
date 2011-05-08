0d63d664ca8269bfdd244588c0a3e0d9491c5ece
Christian Helmich <kurisu.kirin@gmail.com>
2011-05-09 01:02:16 +0200

    added xcodeproj and source files

---
+++ src/kani/core/kaniTypes.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef KANI_TYPES_H
+#define KANI_TYPES_H 1
+
+namespace kani
+{
+	typedef	signed	 char	int8;
+	typedef	unsigned char	uint8;
+	
+	typedef	signed	 short	int16;
+	typedef	unsigned short	uint16;
+	
+	typedef	signed	 int	int32;
+	typedef	unsigned int	uint32;
+	
+	typedef	signed	 long	int64;
+	typedef	unsigned long	uint64;
+	
+	typedef	signed	 float	float32;
+	typedef	signed	 double	float64;
+}
+
+#endif	//KANI_TYPES_H
