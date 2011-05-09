5c36c24854dfb7b9e6be751725207642ffee3813
Christian Helmich <kagekirin@gmail.com>
2011-05-10 00:47:30 +0200

    added kaniUtils.h kaniFileTypes.h

---
+++ src/kani/core/kaniUtils.h
@@ -0,0 +1,7 @@
+//
+//  kaniUtils.h
+//  KaniTexTool
+//
+//  Created by Christian Helmich on 09.05.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
---
+++ src/kani/file/kaniFileTypes.h
@@ -0,0 +1,7 @@
+//
+//  kaniFileTypes.h
+//  KaniTexTool
+//
+//  Created by Christian Helmich on 10.05.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
