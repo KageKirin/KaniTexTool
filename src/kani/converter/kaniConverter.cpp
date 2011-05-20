8e1c0dd336dd23420d8a02434330c8246188f075
Christian Helmich <kagekirin@gmail.com>
2011-05-21 01:17:48 +0200

    added base class for converter
    Signed-off-by: Christian Helmich <kagekirin@gmail.com>

--- src/kani/converter/kaniConverter.cpp
+++ src/kani/converter/kaniConverter.cpp
@@ -6,4 +6,46 @@
 //  Copyright 2011 KageKirin. All rights reserved.
 //
 
-#include "kaniConerter.h"
+#include "kaniConverter.h"
+#include "kaniConverterPVR.h"
+
+namespace kani { namespace converter {
+
+
+	//----------------------------------------------------------------------
+	
+	bool Converter::convert(const string& converter,
+							const string& inputFilename,
+							const string& outputFilename,
+							const string& format,
+							int		mipmaps,
+							bool	regenMips)
+	{
+		return getConverter(converter).internal_convert(inputFilename,
+														outputFilename,
+														format,
+														mipmaps,
+														regenMips);
+	}							
+	
+	//TODO: use as strategy pattern entry later
+	Converter& Converter::getConverter(const string& converter)
+	{
+		static ConverterPVR	cPVR;
+		return cPVR;
+	}
+	
+	//----------------------------------------------------------------------	
+				
+	bool Converter::internal_convert(const string&	inputFilename,
+									 const string&	outputFilename,
+									 const string&	format,
+									 int			mipmaps,
+									 bool			regenMips) const
+	{
+		return false;
+	}
+
+	//----------------------------------------------------------------------
+	
+}}
--- src/kani/converter/kaniConverter.h
+++ src/kani/converter/kaniConverter.h
@@ -10,13 +10,31 @@
 #ifndef	KANI_CONVERTER_H
 #define	KANI_CONVERTER_H	1
 
+#include <string>
+
 namespace kani { namespace converter {
 	
+	using std::string;
+		
 	class Converter
 	{
 	public:
-	
-	
+
+		static bool convert(const string& converter,
+							const string& inputFilename,
+							const string& outputFilename,
+							const string& format,
+							int		mipmaps,
+							bool	regenMips);
+							
+	protected:
+		static Converter& getConverter(const string& converter);
+		
+		virtual bool internal_convert(	const string&	inputFilename,
+										const string&	outputFilename,
+										const string&	format,
+										int				mipmaps,
+										bool			regenMips) const;
 	};
 	
 }}
