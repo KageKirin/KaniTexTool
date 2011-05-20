bca405cc87a474ed150c5a08ba622f98167dc7d3
Christian Helmich <kagekirin@gmail.com>
2011-05-21 01:16:52 +0200

    added tex format enumeration and search functions
    Signed-off-by: Christian Helmich <kagekirin@gmail.com>

--- src/kani/texture/kaniTexFormat.cpp
+++ src/kani/texture/kaniTexFormat.cpp
@@ -7,3 +7,90 @@
 //
 
 #include "kaniTexFormat.h"
+
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <boost/preprocessor/stringize.hpp>
+
+
+namespace kani { namespace texture {
+
+	using std::vector;
+
+	struct TexFormatTuple
+	{
+		const char* text;
+		PixelType	pvrtex;
+		Format		format;
+		
+		TexFormatTuple(const char* t, PixelType p, Format f):
+		text(t), pvrtex(p), format(f)
+		{}
+	};
+	
+	
+#define M_CREATEMAP(r, data, elem)	\
+	s_TexFormatTupleMap.push_back	\
+	( TexFormatTuple(BOOST_PP_STRINGIZE(elem), (PixelType)0x00, BOOST_PP_CAT(data, elem))	);
+	
+	
+	typedef vector<TexFormatTuple>	TexFormatTupleMap;
+	static TexFormatTupleMap s_TexFormatTupleMap;
+	
+	void createTexFormatTupleMap()
+	{
+		static bool created = false;
+		if(created)
+			return;
+			
+		s_TexFormatTupleMap.clear();
+
+		BOOST_PP_SEQ_FOR_EACH(M_CREATEMAP, Format_, FORMAT_SEQ) // Format_x		
+		
+		created = true;
+	}
+	
+	template<typename predicate>
+	const TexFormatTuple& findTuple(predicate P)
+	{
+		createTexFormatTupleMap();
+
+		TexFormatTupleMap::iterator iter = find_if(s_TexFormatTupleMap.begin(), s_TexFormatTupleMap.end(), P);
+		if(iter != s_TexFormatTupleMap.end())
+			return *iter;
+			
+		return s_TexFormatTupleMap.at(0);
+	}
+	
+	struct Predicate_FindByText
+	{
+		const char* text;
+		
+		Predicate_FindByText(const char* t):text(t){}		
+		bool operator()(const TexFormatTuple& tuple)
+		{
+			return (strcmp(text, tuple.text) == 0);
+		}
+	};
+
+	struct Predicate_FindByPVRPixelFormat
+	{
+		PixelType	pixelType;
+		
+		Predicate_FindByPVRPixelFormat(PixelType pt):pixelType(pt){}		
+		bool operator()(const TexFormatTuple& tuple)
+		{
+			return (pixelType == tuple.pvrtex);
+		}
+	};
+	
+	
+	PixelType	getSupportedPixelType(const char* textFormat)
+	{
+		const TexFormatTuple& tuple = findTuple(Predicate_FindByText(textFormat));
+		return tuple.pvrtex;
+	}
+	
+
+}}
--- src/kani/texture/kaniTexFormat.h
+++ src/kani/texture/kaniTexFormat.h
@@ -10,15 +10,66 @@
 #ifndef KANI_TEX_FORMAT
 #define KANI_TEX_FORMAT	1
 
+#include <pvrtex/PVRTexLibGlobals.h>
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/seq/for_each.hpp>
+
 namespace kani { namespace texture {
 
+	using pvrtexlib::PixelType;
+
+
+#define	FORMAT_SEQ	\
+	(SameAsInput)	\
+	(A4R4G4B4)		\
+	(A1R5G5B5)		\
+	(R5G6B5)		\
+	(R5G5B5)		\
+	(A8R8G8B8)		\
+	(A8R3G3B3)		\
+	(I8)			\
+	(A8)			\
+	(A8I8)			\
+	(BITMAP)		\
+	(VY1UY0)		\
+	(Y1VY0U)		\
+	(R4G44BA4)		\
+	(R5G5B5A1)		\
+	(R8G8B8A8)		\
+	(R8G8B8)		\
+	(B8G8R8A8)		\
+	(PVRTC2)		\
+	(PVRTC4)		\
+	(DXT1)			\
+	(DXT2)			\
+	(DXT3)			\
+	(DXT4)			\
+	(DXT5)			\
+	(ETC)			\
+	(R3G3B2)		\
+	(A10B10G10R10)	\
+	(A10R10G10B10)	\
+	(G16R16)		\
+	(V16U16)		\
+	(A16B16G16R16)	\
+	(R16F)			\
+	(GR16F)			\
+	(ABGR16F)		\
+	(R32F)			\
+	(GR32F)			\
+	(ABGR32F)		
+	
+
+#define M_CAT(r, data, elem) BOOST_PP_CAT(data, elem),
+		
 	enum Format
 	{
-		Format_SameAsInput = -1,
-		
+		BOOST_PP_SEQ_FOR_EACH(M_CAT, Format_, FORMAT_SEQ) // Format_x
+		Format_Count		
 	};
-	
-	
+#undef M_CAT	
+
+	PixelType	getSupportedPixelType(const char* textFormat);
 }}
 
 #endif	//KANI_TEX_FORMAT
