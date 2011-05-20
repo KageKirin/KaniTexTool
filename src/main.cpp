b98d4227342c6f2cb13f37534d49b7fb6b5c8b97
Christian Helmich <kagekirin@gmail.com>
2011-05-21 01:18:40 +0200

    updated main
    Signed-off-by: Christian Helmich <kagekirin@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,8 @@
 
 #include <iostream>
 #include <string>
-#include "kani/file/kaniFileTypes.h"
-#include "kani/file/kaniFileHandler.h"
 #include "kani/core/kaniCmdLine.h"
-#include "kani/kaniConversion.h"
-
-#include <pvrtex/CPVRTexture.h>
+#include "kani/converter/kaniConverter.h"
 
 int main (int argc, const char** argv)
 {
@@ -29,47 +25,22 @@ int main (int argc, const char** argv)
 		return 1;
 	}
 	
-	std::cout << "convert " << options.getInputFile() 
-		<< " to " << options.getOutputFile() 
-		<< " with " << options.getFormat()
-		<< " mips " << options.getMipmaps() << std::endl;
-		
-	pvrtexlib::CPVRTextureHeader	texHeader;
-	pvrtexlib::CPVRTextureData		texData;
-
-	std::cout << "read " << kani::file::FileHandler::read(options.getInputFile(), texHeader, texData) << " bytes" << std::endl;
+	std::cout
+		<< "convert "	<< options.getInputFile()
+		<< " to "		<< options.getOutputFile()
+		<< " with "		<< options.getFormat()
+		<< " mips "		<< options.getMipmaps()
+		<< std::endl;
 	
-	std::cout << texHeader.getWidth()	<< std::endl;
-	std::cout << texHeader.getHeight()	<< std::endl;
-
-	kani::uint32 w, h;
-	texHeader.getDimensions(w, h);
-	std::cout << w << " x " << h		<< std::endl;
-
-	std::cout << texHeader.getPixelType()	<< std::endl;
-	std::cout << texHeader.getNumSurfaces()	<< std::endl;
-	std::cout << texHeader.getMipMapCount()	<< std::endl;
+	bool result = kani::converter::Converter::convert(
+		options.getConverter(),
+		options.getInputFile(),
+		options.getOutputFile(),
+		options.getFormat(),
+		options.getMipmaps(),
+		options.getRegenMips()
+		);
 	
-	for(kani::uint32 ml = 0; ml < texHeader.getMipMapCount(); ++ml)
-	{
-		std::cout << "mip " << ml << texHeader.getMipWidth(ml) << " x " << texHeader.getMipHeight(ml) << std::endl;
-	}
-
-	std::cout << "wrote " << kani::file::FileHandler::write(options.getOutputFile(), texHeader, texData) << " bytes" << std::endl;
-
-	
-	//	kani::convert(options.getInputFile(),
-	//			  options.getOutputFile(),
-	//			  options.getFormat(),
-	//			  options.getMipmaps());
-	
-	std::cout << std::hex << kani::file::getFileTypeFromFilename(options.getInputFile()) << std::endl;
-	std::cout << std::hex << kani::file::getFileTypeFromFilename(options.getOutputFile()) << std::endl;
-	
-	std::cout << std::hex << kani::file::FileType_DDS << std::endl;
-	std::cout << std::hex << kani::file::FileType_KTX << std::endl;
-	std::cout << std::hex << kani::file::FileType_PNG << std::endl;
-	std::cout << std::hex << kani::file::FileType_PVR << std::endl;
-    return 0;
+    return result ? 0 : 1;
 }
 
