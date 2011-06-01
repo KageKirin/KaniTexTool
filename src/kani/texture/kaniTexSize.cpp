b49a720d54fdd1fe46a595a9f134e02d90e8c747
Christian Helmich <christianhe@crytek.de>
2011-06-01 12:25:53 +0200

    added kaniTexSize.h/cpp

---
+++ src/kani/texture/kaniTexSize.cpp
@@ -0,0 +1,15 @@
+//
+//  kaniTexSize.cpp
+//  KaniTexTool
+//
+//  Created by Christian Helmich on 6/1/11.
+//  Copyright 2011 KageKirin. All rights reserved.
+//
+
+#include "kaniTexSize.h"
+
+namespace kani { namespace texture {
+	
+	
+	
+}}
---
+++ src/kani/texture/kaniTexSize.h
@@ -0,0 +1,19 @@
+//
+//  kaniTexSize.h
+//  KaniTexTool
+//
+//  Created by Christian Helmich on 6/1/11.
+//  Copyright 2011 KageKirin. All rights reserved.
+//
+
+#pragma once
+#ifndef KANI_TEX_SIZE_H
+#define KANI_TEX_SIZE_H	1
+
+namespace kani { namespace texture {
+
+	
+	
+}}
+
+#endif	//KANI_TEX_SIZE_H
