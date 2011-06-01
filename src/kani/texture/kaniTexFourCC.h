91975d0af867cfbb62caf2a461eb0cb73c5c1fc4
Christian Helmich <christianhe@crytek.de>
2011-06-01 12:21:56 +0200

    changed FourCC enum to PrePro constructedenum

--- src/kani/texture/kaniTexFourCC.h
+++ src/kani/texture/kaniTexFourCC.h
@@ -13,24 +13,55 @@
 #include "../core/kaniTypes.h"
 #include "../core/kaniEndian.h"
 
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/preprocessor/tuple/elem.hpp>
+
 namespace kani { namespace texture {
 	
 	using kani::uint32;
 	using kani::endian::static_swap;
 
-	//dds standard fourcc's
-	static const uint32 FourCC_DXT1 = static_swap<uint32>::swap<uint32('DXT1')>::value;
-	static const uint32 FourCC_DXT2 = static_swap<uint32>::swap<uint32('DXT2')>::value;
-	static const uint32 FourCC_DXT3 = static_swap<uint32>::swap<uint32('DXT3')>::value;
-	static const uint32 FourCC_DXT4 = static_swap<uint32>::swap<uint32('DXT4')>::value;
-	static const uint32 FourCC_DXT5 = static_swap<uint32>::swap<uint32('DXT5')>::value;
+#define	MAKE_FOURCC(fourcc)							static_swap<uint32>::swap<uint32(fourcc)>::value
+#define	MAKE_FOURCC_ENUM_VALUE(base, name, fourcc)	BOOST_PP_CAT(base, name) = MAKE_FOURCC(fourcc)
+
+#define M_MAKE_FOURCC_ENUM(r, data, elem)	\
+	MAKE_FOURCC_ENUM_VALUE(data,			\
+		BOOST_PP_TUPLE_ELEM(2, 0, elem),	\
+		BOOST_PP_TUPLE_ELEM(2, 1, elem)		\
+	),										\
+
+
+//dds standard fourcc's
+#define DDS_FOURCC_SEQ	\
+	((DXT1, 'DXT1'))	\
+	((DXT2, 'DXT2'))	\
+	((DXT3, 'DXT3'))	\
+	((DXT4, 'DXT4'))	\
+	((DXT5, 'DXT5'))	\
+
 	
-	//non-standard fourcc's
-	static const uint32 FourCC_PVR2 = static_swap<uint32>::swap<uint32('PVR2')>::value;
-	static const uint32 FourCC_PVR4 = static_swap<uint32>::swap<uint32('PVR4')>::value;
-	static const uint32 FourCC_ETC1 = static_swap<uint32>::swap<uint32('ETC1')>::value;
+//non-standard fourcc's	
+#define	PVR_FOURCC_SEQ	\
+	((PVR2, 'PVR2'))	\
+	((PVR4, 'PVR4'))	\
+	((ETC1, 'ETC1'))	\
+
+//extend here
+#define	EXT_FOURCC_SEQ	\
+
+
+
+#define FOURCC_SEQ	\
+	DDS_FOURCC_SEQ	\
+	PVR_FOURCC_SEQ	\
+	EXT_FOURCC_SEQ	\
+
 	
-	//extend here
+	enum FourCC
+	{
+		BOOST_PP_SEQ_FOR_EACH(M_MAKE_FOURCC_ENUM, FourCC_, FOURCC_SEQ) 
+	};
 	
 }}
 
